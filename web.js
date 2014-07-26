1a6e2145a084fe76b8e20e672378141dd6fd2074
Kyle McDonald <kyle@kylemcdonald.net>
2014-07-26 16:32:40 -0400

    origins

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(process.env.PORT || 5000);
+server.listen(process.env.PORT || 5000, { origins: '*:*' });
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
