80ba7b9f66d253ff50e308642bbe7f50860e0818
Kyle McDonald <kyle@kylemcdonald.net>
2014-07-26 16:19:43 -0400

    100ms

--- web.js
+++ web.js
@@ -1,14 +1,19 @@
-var express = require("express");
-var logfmt = require("logfmt");
-var app = express();
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-app.use(logfmt.requestLogger());
+server.listen(process.env.PORT || 5000);
 
-app.get('/', function(req, res) {
-  res.send('<h1 style="font-family: sans-serif">uzi.</h1>');
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
 });
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
+io.on('connection', function (socket) {
+  socket.emit('log', { hello: 'connected' });
 });
+
+function sendTime() {
+    io.sockets.emit('time', { time: new Date().toJSON() });
+}
+
+setInterval(sendTime, 100);
