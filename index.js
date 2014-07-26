cbe03e2aa82fb32998b445cf1ce5ae1ac9327554
Kyle McDonald <kyle@kylemcdonald.net>
2014-07-26 16:50:26 -0400

    span

--- index.js
+++ index.js
@@ -9,20 +9,12 @@ app.use(express.static(__dirname + "/"))
 var server = http.createServer(app)
 server.listen(port)
 
-console.log("http server listening on %d", port)
-
 var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
-
 wss.on("connection", function(ws) {
   var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
-
-  console.log("websocket connection open")
-
+    ws.send("pew", function() { })
+  }, 100)
   ws.on("close", function() {
-    console.log("websocket connection close")
     clearInterval(id)
   })
 })
